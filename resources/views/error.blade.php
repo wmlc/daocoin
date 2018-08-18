24dbbbf9bb4e4b95f0d24867d67712aad8849d8b
wangmaolin <wangmaolin@wukongtv.com>
2018-08-18 16:39:45 +0800

    no message

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -3,7 +3,7 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="text-center" style="padding:200px 0;">
-        <img style="margin-bottom:30px;" src="/assets/img/error.png"/>
+        <img style="margin-bottom:30px;" src="/assets/img/warn_fill.png"/>
         <p>{{$message}}</p>
     </div>
 
--- resources/views/kycSuccess.blade.php
+++ resources/views/kycSuccess.blade.php
@@ -3,7 +3,7 @@
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <div class="text-center" style="padding:200px 0;">
-        <img style="margin-bottom:30px;" src="/assets/img/error.png"/>
+        <img style="margin-bottom:30px;" src="/assets/img/round_check_fill.png"/>
         <p>Your KYC/AML certification is successful!
            Now you can start the first stroke <a href="/buy">Convert fiat to Token</a></p>
     </div>
